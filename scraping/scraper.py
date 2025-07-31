82f10f3c3b60fa0e8f936f6ead5a1b276c248459
João Vitor <wenes.vargas10@gmail.com>
2025-07-31 02:11:16 -0300

    Projeto atualizado

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -38,4 +38,4 @@ print(assisentencias_por_jogo.text)
 ratings= whoscored.find_element(By.XPATH,'/html/body/div[4]/div[3]/div[1]/div[11]/div/div[4]')
 print(ratings.text) 
 
-quit= whoscored.quit()
+quit = whoscored.quit()
