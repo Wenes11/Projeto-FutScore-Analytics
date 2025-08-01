07219045156227804ab9432c1df058fd1b0080a0
João Vitor <wenes.vargas10@gmail.com>
2025-08-01 00:52:33 -0300

    Coletei os dados, e coloquei em uma tabela usando o pandas, ele pegou só a primeira tabela

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,41 +1,68 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import pandas as pd
 import time
 
+# Iniciar o navegador
 whoscored = webdriver.Chrome()
 
+# Acessar o site
 whoscored.get('https://br.whoscored.com/statistics')
+time.sleep(3)
 
-clicar = whoscored.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div[2]/div/button')
+# Aceitar cookies
+clicar = whoscored.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div[2]/div/button')
 clicar.click()
+time.sleep(1)
 
-pesquisar = whoscored.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div[1]/div/div[2]/div/input')
+# Pesquisar pelo Brasileirão
+pesquisar = whoscored.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div[2]/div/input')
 pesquisar.send_keys('Brasileirao')
 pesquisar.send_keys(Keys.ENTER)
+time.sleep(2)
 
-brasileirao = whoscored.find_element(By.XPATH,'/html/body/div[4]/div[3]/div[1]/div[2]/table/tbody/tr[2]/td[1]/a')
+# Clicar no link do Brasileirão
+brasileirao = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[2]/table/tbody/tr[2]/td[1]/a')
 brasileirao.click()
+time.sleep(5)
 
-tabale_Brasileirao = whoscored.find_element(By.XPATH,'//*[@id="standings-24121"]/div[2]')
-print(tabale_Brasileirao.text)
+# Coletar elementos
+tabela_Brasileirao = whoscored.find_element(By.XPATH, '//*[@id="standings-24121"]/div[2]')
+print("Tabela:\n", tabela_Brasileirao.text)
 
-jogos_Brasileirao = whoscored.find_element(By.XPATH,'/html/body/div[4]/div[3]/div[1]/div[4]/div[2]/div/div[4]/div/div')
-print(jogos_Brasileirao.text)
+jogos_Brasileirao = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[4]/div[2]/div/div[4]/div/div')
+print("\nJogos:\n", jogos_Brasileirao.text)
 
-melhores_timesBrsasileirao = whoscored.find_element(By.XPATH,'/html/body/div[4]/div[3]/div[1]/div[9]')
-print(melhores_timesBrsasileirao.text)
+melhores_times_Brasileirao = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[9]')
+print("\nMelhores Times:\n", melhores_times_Brasileirao.text)
 
-artilheiros_Brasileirao = whoscored.find_element(By.XPATH,'/html/body/div[4]/div[3]/div[1]/div[11]/table')
-print(artilheiros_Brasileirao.text)
+artilheiros_Brasileirao = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[11]/table')
+print("\nArtilheiros:\n", artilheiros_Brasileirao.text)
 
-chutes_por_jogo = whoscored.find_element(By.XPATH,'/html/body/div[4]/div[3]/div[1]/div[11]/div/div[2]')
-print(chutes_por_jogo.text)
+chutes_por_jogo = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[11]/div/div[2]')
+print("\nChutes por jogo:\n", chutes_por_jogo.text)
 
-assisentencias_por_jogo = whoscored.find_element(By.XPATH,'//html/body/div[4]/div[3]/div[1]/div[11]/div/div[3]')
-print(assisentencias_por_jogo.text)
+assistencias_por_jogo = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[11]/div/div[3]')
+print("\nAssistências por jogo:\n", assistencias_por_jogo.text)
 
-ratings= whoscored.find_element(By.XPATH,'/html/body/div[4]/div[3]/div[1]/div[11]/div/div[4]')
-print(ratings.text) 
+ratings = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[11]/div/div[4]')
+print("\nRatings:\n", ratings.text)
 
-quit = whoscored.quit()
+# Coletar as tabelas com pandas
+# ...existing code...
+
+# Coletar as tabelas com pandas
+html = whoscored.page_source
+tabelas = pd.read_html(html)
+
+# Salvar a primeira tabela capturada no arquivo Dados_Brasileirao.xlsx
+tabelas[0].to_excel('Dados_Brasileirao.xlsx', index=False)
+
+# Exemplo: Exibir as tabelas capturadas
+for i, tabela in enumerate(tabelas):
+    print(f"\nTabela {i+1}:\n", tabela)
+
+# Finalizar o navegador
+whoscored.quit()
+# ...existing code...
