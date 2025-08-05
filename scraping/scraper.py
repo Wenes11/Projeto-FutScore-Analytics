dff54e7d853ab54d7ff3c35e98b19b357880b42f
João Vitor <wenes.vargas10@gmail.com>
2025-08-04 22:35:06 -0300

    Baixei extençoes excel viewer dei um pip install beautifuls4

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -17,11 +17,12 @@ clicar = whoscored.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/
 clicar.click()
 time.sleep(1)
 
+
 # Pesquisar pelo Brasileirão
 pesquisar = whoscored.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div[2]/div/input')
 pesquisar.send_keys('Brasileirao')
 pesquisar.send_keys(Keys.ENTER)
-time.sleep(2)
+time.sleep(3)
 
 # Clicar no link do Brasileirão
 brasileirao = whoscored.find_element(By.XPATH, '/html/body/div[4]/div[3]/div[1]/div[2]/table/tbody/tr[2]/td[1]/a')
